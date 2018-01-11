aadf0b714b8d7f478d1dac6810eac75080f5fdbf
Will Qian <qianwent@hotmail.com>
2018-01-11 09:48:32 -0500

    spring interceptor example

--- src/main/java/com/qwt/springmaven/config/SpringMvcConfig.java
+++ src/main/java/com/qwt/springmaven/config/SpringMvcConfig.java
@@ -1,8 +1,12 @@
 package com.qwt.springmaven.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.mvc.WebContentInterceptor;
 
 /**
  * This class is also used to replace applicationContext.xml where <mvc:annotation-driven/> is defined
@@ -10,4 +14,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @EnableWebMvc
 @Configuration
 public class SpringMvcConfig extends WebMvcConfigurerAdapter {
+
+    /**
+     * To address IE issues with no-store, replace default cache parameters with one that
+     * does not include no-store. See https://github.com/FortAwesome/Font-Awesome/issues/6454
+     */
+    @Bean
+    public WebContentInterceptor webContentInterceptor() {
+        WebContentInterceptor interceptor = new WebContentInterceptor();
+        CacheControl cacheControl = CacheControl.noCache().mustRevalidate().cachePrivate();
+        interceptor.setCacheControl(cacheControl);
+        return interceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(webContentInterceptor());
+    }
+
 }
