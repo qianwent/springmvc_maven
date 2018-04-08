a786efc761857b6e8ae3fb9a47a564f62e5244fe
Will Qian <qianwent@hotmail.com>
2018-04-08 11:03:16 -0400

    more unit test, and more comments

--- src/test/java/com/qwt/springmaven/customannotation/CustomAnnotationTest.java
+++ src/test/java/com/qwt/springmaven/customannotation/CustomAnnotationTest.java
@@ -2,14 +2,14 @@ package com.qwt.springmaven.customannotation;
 
 import com.qwt.springmaven.config.AppConfig;
 import com.qwt.springmaven.service.MyService;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Wentao Qian on 3/13/2018.
@@ -26,9 +26,16 @@ public class CustomAnnotationTest {
 
     //to make AOP working, we need to use @EnableAspectJAutoProxy in config class, it's very important!
     @Test(expected = IllegalArgumentException.class)
-    public void test() {
+    public void testApplyServiceWithNullValue() {
         String caller = null;
         myService.applyService(caller);
+    }
+
+    @Test
+    public void testApplyServiceWithNonNullValue() {
+        String caller = "aaa";
+        String servicResult = myService.applyService(caller);
+        assertEquals(servicResult, "aaa");
     }
 
 }
