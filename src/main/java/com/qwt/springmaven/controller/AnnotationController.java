96333f368458fc812f524053e1058266e4df0d9b
Will Qian <qianwent@hotmail.com>
2018-08-18 18:49:31 -0400

    Add consumes and produces type for controller.

--- src/main/java/com/qwt/springmaven/controller/AnnotationController.java
+++ src/main/java/com/qwt/springmaven/controller/AnnotationController.java
@@ -2,6 +2,7 @@ package com.qwt.springmaven.controller;
 
 import com.qwt.springmaven.service.MyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,13 +21,14 @@ public class AnnotationController {
         this.myService = myService;
     }
 
-    @RequestMapping(value = "/annotation", method = RequestMethod.POST)
+    @RequestMapping(value = "/annotation", method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public String callMyService() {
-        //TODO: for testing purpose, we use null value here. need to change to use request object
-        String caller = null;
-//        String caller = "test";
+    public String callMyService(String caller) {
+        //if testing exception, then do not pass caller.
         return myService.applyService(caller);
+        //TODO: need common web response, and global exception handler
     }
 
 }
