be6560a185f21b8de42d117009240a2dafa66982
Will Qian <qianwent@hotmail.com>
2018-09-30 21:42:51 -0400

    spring cache usage.

--- src/main/java/com/qwt/springmaven/service/MyService.java
+++ src/main/java/com/qwt/springmaven/service/MyService.java
@@ -1,6 +1,7 @@
 package com.qwt.springmaven.service;
 
 import com.qwt.springmaven.annotation.CustomService;
+import org.springframework.cache.annotation.Cacheable;
 
 import javax.validation.constraints.NotNull;
 
@@ -13,7 +14,9 @@ public class MyService {
     //@NotNull在本身并不起任何作用，只是一个标识，类似于文档说明，表明这个地方“不应该是空”
     //https://stackoverflow.com/questions/34094039/using-notnull-annotation-in-method-argument
     //这里有详细说明
+    @Cacheable(value = "userCache", condition = "#caller.length()==3")
     public String applyService(@NotNull String caller) {
+        System.out.println("calling method, not cache...");
         return caller;
     }
 
